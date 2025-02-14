7f1fc3b8870442bca92d3a0af0a7159003c3aec7
thefayras <dimitri.tarn@gmail.com>
2025-02-14 14:28:53 +0100

    Refactoring: Klasse für "ScrollableText" implementiert und Scrolling in die Klasse verlagert.

--- now_playing/__init__.py
+++ now_playing/__init__.py
@@ -5,8 +5,8 @@ import time
 
 from rich.live import Live
 from rich.text import Text
-from rich.cells import cell_len
-from rich.markup import escape as rich_escape
+
+from now_playing.scrollable_text import ScrollableText
 from now_playing.spotify_token import SpotifyToken
 from now_playing.progress_bar import ProgressBar
 
@@ -26,35 +26,6 @@ def get_current_song(token):
     current_song_info["duration"] = now_playing_response["item"]["duration_ms"]
 
 
-def print_text(label, text, style):
-    return f"{label}: [{style}]{rich_escape(text)}[/{style}]\n"
-
-
-def scroll_text(width, amount, label, text, style):
-    total_length = cell_len(label) + cell_len(text) + cell_len(": ")
-
-    # If text fits entirely, no need for scrolling
-    if total_length <= width:
-        return print_text(label, text, style)
-
-    # Calculate how many positions we need to scroll through
-    scroll_length = total_length - width + 2
-    scroll_position = amount % (scroll_length + 2)
-
-    # Add pause by repeating the first and last position
-    if scroll_position == 0:  # Pause at start
-        n = 0
-    elif scroll_position == scroll_length + 1:  # Pause at end
-        n = scroll_length - 1
-    else:  # Normal scrolling
-        n = scroll_position - 1
-
-    ellipsis = "" if total_length == width + n - 1 else "…"
-    available_width = width - cell_len(label) - cell_len(": ") - cell_len(ellipsis)
-    text = text[0 + n:available_width + n - 1].rstrip(" ") + ellipsis
-    return print_text(label, text, style)
-
-
 def show_now_playing():
     token = SpotifyToken.get()
 
@@ -65,6 +36,8 @@ def show_now_playing():
 
     elapsed_time = 0
     get_current_song(token)
+    title = ScrollableText("Title")
+    artist = ScrollableText("Artist")
     progress_bar = ProgressBar()
     with Live(rich_text, auto_refresh=False, console=console) as live:
         live.console.clear()
@@ -76,8 +49,8 @@ def show_now_playing():
                 get_current_song(token)
 
             width = live.console.size.width
-            title = scroll_text(width, elapsed_time, "Title", current_song_info["name"], "bold dark_red")
-            artist = scroll_text(width, elapsed_time, "Artist", current_song_info["artists"], "bold dark_red")
+            title.update(current_song_info["name"], width).scroll()
+            artist.update(current_song_info["artists"], width).scroll()
             progress_bar.update(live.console, current_song_info["progress"], current_song_info["duration"])
             rich_text = Text.from_markup(f"{title}{artist}{progress_bar}")
 
--- now_playing/scrollable_text.py
+++ now_playing/scrollable_text.py
@@ -1,9 +1,67 @@
-from rich.text import Text
+from rich.cells import cell_len
+from rich.markup import escape as rich_escape
 
-# TODO
-class ScrollableText(Text):
-    def __init__(self, title, scrollable_text, *args, **kwargs) -> None:
-        super().__init__()
 
-    def scroll(self, amount):
-        pass
+class ScrollableText:
+    def __init__(self, title, scrollable_text = "", width = 0, style = "bold dark_red") -> None:
+        self.title = title
+        self.seperator = ": " if title != "" else ""
+        self.scrollable_text = scrollable_text
+        self.current_text = scrollable_text
+        self.width = width
+        self.style = style
+        self.offset = 0
+        self.title_length = None
+        self.text_length = None
+        self.seperator_length = None
+        self.update_lengths()
+
+    def scroll(self, amount = 1):
+        total_length = self.title_length + self.text_length + self.seperator_length
+
+        # If text fits entirely, no need for scrolling
+        if total_length <= self.width:
+            return
+
+        # Calculate how many positions we need to scroll through
+        scroll_length = total_length - self.width + 2
+        scroll_position = self.offset % (scroll_length + 2)
+        self.offset += amount
+
+        # Add pause by repeating the first and last position
+        if scroll_position == 0:  # Pause at start
+            n = 0
+        elif scroll_position == scroll_length + 1:  # Pause at end
+            n = scroll_length - 1
+        else:  # Normal scrolling
+            n = scroll_position - 1
+
+        ellipsis = "" if total_length == self.width + n - 1 else "…"
+        available_width = self.width - self.title_length - self.seperator_length - cell_len(ellipsis)
+        self.current_text = self.scrollable_text[0 + n:available_width + n - 1].rstrip(" ") + ellipsis
+
+    def update_lengths(self):
+        self.title_length = cell_len(self.title)
+        self.text_length = cell_len(self.scrollable_text)
+        self.seperator_length = cell_len(self.seperator)
+
+    def update_text(self, text):
+        if self.scrollable_text != text:
+            self.offset = 0
+
+        self.scrollable_text = text
+        self.current_text = text
+        self.update_lengths()
+
+    def update_width(self, width):
+        self.width = width
+
+    def update(self, text, width):
+        self.update_text(text)
+        self.update_width(width)
+        self.update_lengths()
+        return self
+
+    def __str__(self):
+        return f"{self.title}: [{self.style}]{rich_escape(self.current_text)}[/{self.style}]\n"
+
