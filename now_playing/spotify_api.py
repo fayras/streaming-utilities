8fbe67ee73d1341f054d5725fc0b6579d0c89b5b
thefayras <dimitri.tarn@gmail.com>
2025-03-04 19:35:28 +0100

    SpotifyAPI: Refactoring des Error Handlings

--- now_playing/spotify_api.py
+++ now_playing/spotify_api.py
@@ -16,43 +16,35 @@ class SpotifyAPI:
         self.token = SpotifyToken.get()
 
     def get_currently_playing(self):
-        response = self.do_request(
-            "https://api.spotify.com/v1/me/player/currently-playing")
+        api_url = "https://api.spotify.com/v1/me/player/currently-playing"
+        response = self.do_request(api_url)
 
-        if response["currently_playing_type"] == "track":
-            return Song(
-                response["item"]["name"],
-                ", ".join([a["name"] for a in response["item"]["artists"]]),
-                response["progress_ms"],
-                response["item"]["duration_ms"]
-            )
+        if response.status_code == 200:
+            response = response.json()
+            if response["currently_playing_type"] == "track":
+                return Song(
+                    response["item"]["name"],
+                    ", ".join([a["name"] for a in response["item"]["artists"]]),
+                    response["progress_ms"],
+                    response["item"]["duration_ms"]
+                )
 
         return None
 
     def get_headers(self):
-        return {
-            "Authorization": f"{self.token.token_type} {self.token.token}"
-        }
+        return {"Authorization": f"{self.token.token_type} {self.token.token}"}
 
-    def do_request(self, url, count=0):
-        response = requests.get(
-            url=url,
-            headers=self.get_headers(),
-        )
-
-        if count > 2:
-            Exception("do_request called too many times. " + response.text)
-
-        if response.status_code == 200:
-            return response.json()
-
-        if response.status_code == 401:  # 401 = Unauthenticated
+    def check_token(self):
+        if not self.token.is_valid():
             try:
                 self.token.refresh()
             except Exception:
                 self.token = SpotifyToken.get()
 
-            return self.do_request(url, count + 1)
+    def do_request(self, url, method=requests.get, params=None):
+        self.check_token()
 
-        raise Exception(
-            "Something went wrong while doing request to Spotify. " + response.text)
+        if params is None:
+            params = {}
+
+        return method(url=url, params=params, headers=self.get_headers())
