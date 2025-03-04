440aab35a1c22ede98ca364be70a019bb617f0c9
thefayras <dimitri.tarn@gmail.com>
2025-03-04 20:20:11 +0100

    Fix: Response muss als JSON geparsed werden

--- now_playing/spotify_api.py
+++ now_playing/spotify_api.py
@@ -36,8 +36,11 @@ class SpotifyAPI:
         api_url = "https://api.spotify.com/v1/me/player/queue"
 
         response = self.do_request(api_url, requests.post, {"uri": full_id})
+
         if not response.status_code == 200:
-            print("Could not queue song")
+            return False, response.json()
+
+        return True, "OK"
 
     def get_headers(self):
         return {"Authorization": f"{self.token.token_type} {self.token.token}"}
