cd0a0637fbb8161a6188edbe946eb9eb199c70d7
thefayras <dimitri.tarn@gmail.com>
2025-03-16 18:17:56 +0100

    Coffee Command hinzugefügt

---
+++ commands/coffee_command.py
@@ -0,0 +1,22 @@
+from typing import Self, Any
+
+import twitchAPI.chat
+
+from commands.base_command import BaseCommand, ChatUser
+
+
+class CoffeeCommand(BaseCommand):
+    name = "coffee"
+    user_cooldown = 30
+
+    def execute(self, chat_message: twitchAPI.chat.ChatMessage) -> None:
+        pass
+
+    def parse(self, params: list[str], user: ChatUser) -> Self | None:
+        return self
+
+    def set_params_from_json(self, json: dict[str, Any]) -> Self | None:
+        return self
+
+    def get_params(self) -> dict[str, Any]:
+        pass
