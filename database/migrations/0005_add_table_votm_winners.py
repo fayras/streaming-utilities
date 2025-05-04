da2b53e2c9f2382aab93c5da0b543df747932682
thefayras <dimitri.tarn@gmail.com>
2025-05-04 16:47:17 +0200

    Tabelle für VOTM Winners hinzugefügt

---
+++ database/migrations/0005_add_table_votm_winners.py
@@ -0,0 +1,21 @@
+from sqlite3 import Connection
+from database.DatabaseMigration import DatabaseMigration
+
+
+class AddTableVOTMWinners(DatabaseMigration):
+    def up(self, connection: Connection) -> None:
+        cursor = connection.cursor()
+        cursor.execute('''
+                CREATE TABLE viewer_of_the_month_winners (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    challenge_id INTEGER,
+                    user_id INTEGER,
+                    timestamp TIMESTAMP default CURRENT_TIMESTAMP,
+                    FOREIGN KEY (challenge_id) REFERENCES viewer_of_the_month_challenges(id),
+                    FOREIGN KEY (user_id) REFERENCES users(id)
+                )
+                ''')
+
+    def down(self, connection: Connection) -> None:
+        cursor = connection.cursor()
+        cursor.execute("DROP TABLE viewer_of_the_month_winners")
